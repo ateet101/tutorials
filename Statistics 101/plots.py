43e346936f99ece9e2855fafa979a8bea312f08d
ateet <ateet101@gmail.com>
2018-04-12 23:01:45 -0400

    Completed Z test Examples

--- Statistics 101/plots.py
+++ Statistics 101/plots.py
@@ -1,4 +1,4 @@
-import matplotlib.pyplot
+import matplotlib.pyplot as plt
 import numpy as np
 
 class Plots:
@@ -8,17 +8,41 @@ class Plots:
         self.y = y
 
     def test(self):
-    	print('hhjkk')
+        print(str(self.x))
 
-    def add_plots(self, lower, middle, high):
-    	between = np.where(np.round(x) == middle[0])[0][313]
-		between1 = np.where(np.round(x) == middle[1])[0][313]
-		color1 = np.where(np.round(x) == lower[0])[0][0]
-		color2 = np.where(np.round(x) == lower[1])[0][313]
-		color3 = np.where(np.round(x) == high[0])[0][313]
-		color4 = np.where(np.round(x) == high[1])[0][-1]
-		fig, ax1 = plt.subplots(1, 1, sharex=True)
-		ax1.plot(x,y)
-		ax1.fill_between(x[between:between1], 0, y[between:between1])
-		ax1.fill_between(x[color1:color2], 0, y[color1:color2], facecolor ='green')
-		ax1.fill_between(x[color3:color4], 0, y[color3:color4], facecolor ='green')
+    def compute(self, lower, middle, high):
+        between = []
+        color = []
+        between.append(np.where(np.round(self.x) == middle[0])[0][313])
+        between.append(np.where(np.round(self.x) == middle[1])[0][313])
+        color.append(np.where(np.round(self.x) == lower[0])[0][0])
+        color.append(np.where(np.round(self.x) == lower[1])[0][313])
+        color.append(np.where(np.round(self.x) == high[0])[0][313])
+        color.append(np.where(np.round(self.x) == high[1])[0][-1])
+        return between, color
+
+    def add_plots(self,  ax, low, middle, high, plot_title):
+        color_in, color_out = self.compute(low, middle, high)
+        ax.plot(self.x, self.y)
+        ax.fill_between(self.x[color_in[0]:color_in[1]], 0, self.y[color_in[0]:color_in[1]])
+        ax.fill_between(self.x[color_out[0]:color_out[1]], 0, self.y[color_out[0]:color_out[1]], facecolor ='green')
+        ax.fill_between(self.x[color_out[2]:color_out[3]], 0, self.y[color_out[2]:color_out[3]], facecolor ='green')
+        ax.set_title(plot_title)
+        return ax
+
+    def compute_two(self, lower, high):
+        color = []
+        color.append(np.where(np.round(self.x, 1) == lower[0])[0][0])
+        color.append(np.where(np.round(self.x, 1) == lower[1])[0][30])
+        color.append(np.where(np.round(self.x, 1) == high[0])[0][30])
+        color.append(np.where(np.round(self.x, 1) == high[1])[0][-1])
+        return color
+
+    def add_plots_two(self,  ax, low, high, plot_title):
+        color_out = self.compute_two(low, high)
+        ax.plot(self.x, self.y)
+        ax.fill_between(self.x, 0, self.y, facecolors="blue")
+        ax.fill_between(self.x[color_out[0]:color_out[1]], 0, self.y[color_out[0]:color_out[1]], facecolor ='green')
+        ax.fill_between(self.x[color_out[2]:color_out[3]], 0, self.y[color_out[2]:color_out[3]], facecolor ='green')
+        ax.set_title(plot_title)
+        return ax
